280d3e43c96b15e06da3aa37f5f024543ccec0c5
Altair_As <semalexandrov@gmail.com>
2024-09-28 02:35:33 +0300

    Added question interface and repository

---
+++ SurveyApi/Repositories/IQuestionRepository.cs
@@ -0,0 +1,9 @@
+﻿using SurveyApi.Entities;
+
+namespace SurveyApi.Repositories
+{
+    public interface IQuestionRepository
+    {
+        Task<Question?> GetQuestionByIdAsync(int questionId);
+    }
+}
---
+++ SurveyApi/Repositories/QuestionRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveyApi.Data;
+using SurveyApi.Entities;
+
+namespace SurveyApi.Repositories
+{
+    public class QuestionRepository(AppDbContext context) : IQuestionRepository
+    {
+        public async Task<Question?> GetQuestionByIdAsync(int questionId)
+        {
+            return await context.Questions
+                .Include(q => q.Answers)
+                .FirstOrDefaultAsync(q => q.Id == questionId);
+        }
+    }
+}
