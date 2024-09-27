64ff557d262969d2baed35400ecf42639c597428
Altair_As <semalexandrov@gmail.com>
2024-09-27 03:52:25 +0300

    Created database entities

---
+++ SurveyApi/Entities/Answer.cs
@@ -0,0 +1,11 @@
+﻿namespace SurveyApi.Entities
+{
+    public class Answer
+    {
+        public int Id { get; set; }
+        public required string Text { get; set; }
+
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
---
+++ SurveyApi/Entities/Question.cs
@@ -0,0 +1,15 @@
+﻿namespace SurveyApi.Entities
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public required string Text { get; set; }
+        public int QuesionOrder { get; set; }
+
+        public int SurveyId { get; set; }
+        public Survey Survey { get; set; }
+
+        public ICollection<Answer> Answers { get; set; } = new List<Answer>();
+
+    }
+}
