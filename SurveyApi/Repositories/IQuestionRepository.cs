428d4b807796e6ffc2ae867d1951240277c628d8
Altair_As <semalexandrov@gmail.com>
2024-09-28 04:37:25 +0300

    Updated question interface and repository

--- SurveyApi/Repositories/IQuestionRepository.cs
+++ SurveyApi/Repositories/IQuestionRepository.cs
@@ -4,6 +4,8 @@ namespace SurveyApi.Repositories
 {
     public interface IQuestionRepository
     {
+        Task<int> GetNextQuestionId(int questionId);
+        Task<int> GetFirstQuestionIdBySurveyId(int surveyId);
         Task<Question?> GetQuestionByIdAsync(int questionId);
     }
 }
--- SurveyApi/Repositories/QuestionRepository.cs
+++ SurveyApi/Repositories/QuestionRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
 using SurveyApi.Data;
 using SurveyApi.Entities;
 
@@ -6,6 +7,34 @@ namespace SurveyApi.Repositories
 {
     public class QuestionRepository(AppDbContext context) : IQuestionRepository
     {
+        public async Task<int> GetNextQuestionId(int questionId)
+        {
+            var currentQuestion = await context.Questions
+                .Where(q => q.Id == questionId)
+                .Select(q => new { q.SurveyId, q.QuesionOrder })
+                .FirstOrDefaultAsync()
+                ?? throw new InvalidOperationException($"Unable to find question with id: {questionId}");
+
+            var nextQuestionId = await context.Questions
+                .Where (q => q.SurveyId == currentQuestion.SurveyId && q.QuesionOrder > currentQuestion.QuesionOrder)
+                .OrderBy(q => q.QuesionOrder)
+                .Select(q => q.Id)
+                .FirstOrDefaultAsync();
+
+            return nextQuestionId == 0 ? -1 : nextQuestionId;
+        }
+
+        public async Task<int> GetFirstQuestionIdBySurveyId(int surveyId)
+        {
+            var firstQuestionId = await context.Questions
+                .Where(q => q.SurveyId == surveyId)
+                .OrderBy(q => q.QuesionOrder)
+                .Select(q => q.Id)
+                .FirstOrDefaultAsync();
+
+            return firstQuestionId == 0 ? -1 : firstQuestionId;
+        }
+
         public async Task<Question?> GetQuestionByIdAsync(int questionId)
         {
             return await context.Questions
